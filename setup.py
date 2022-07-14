d2c0dda5331197dda0af5342c75ce57d7bba4fdf
Thiago Badaró <thiago.saramela@usp.br>
2022-07-14 09:30:32 -0300

    Using new setup script

--- setup.py
+++ setup.py
@@ -6,20 +6,24 @@ import sys
 
 def install_packages(load_libs=False):
     if not os.path.isfile("/content/env.tar.gz"):
-        print("Downloading binaries...")
+        print("Downloading binaries...", end=" ")
         subprocess.run(['wget', '-O', '/content/env.tar.gz', 'https://www.dropbox.com/s/wb9z69d9cbelyqq/env.tar.gz'],
                        check=True, text=True)
+        print("✔️")
 
-        print("Unpacking...")
+        print("Extracting files...", end=" ")
         subprocess.run(['tar', '-xf' 'env.tar.gz', '-C', '/content'],
                        check=True, text=True)
+        print("✔️")
 
-        print("Installing dependencies....")
+        print("Installing dependencies....", end=" ")
         subprocess.run(['apt-get', 'install', 'libgsl-dev'],
                        check=True, text=True)
         subprocess.run(['pip3', 'install', '--upgrade', 'gmsh'],
                        check=True, text=True)
+        print("✔️")
 
+    print("Loading libs...", end=" ")
     # Set the environment variables.
     os.environ["GARFIELD_HOME"] = "/content/garfield/garfieldpp"
     os.environ["ROOTSYS"] = "/content/root/install"
@@ -45,7 +49,9 @@ def install_packages(load_libs=False):
     ctypes.cdll.LoadLibrary('/content/root/install/lib/libGpad.so')
     ctypes.cdll.LoadLibrary('/content/root/install/lib/libGraf3d.so')
     ctypes.cdll.LoadLibrary('/content/root/install/lib/libcppyy_backend3_7.so')
+    print("✔️")
 
     import ROOT
     ROOT.gSystem.Load("/content/garfield/install/lib/libmagboltz.so")
     ROOT.gSystem.Load("/content/garfield/install/lib/libGarfield.so")
+    print("All done! 🚀")
